8f614b9b2b963e20c878d56ccd037e8758120ae8
Hardik-G14 <hardikgathibandhe14@gmail.com>
2025-11-06 17:58:15 +0530

    Added new files

---
+++ src/graph/state/graph_state.py
@@ -0,0 +1,15 @@
+from typing import TypedDict
+
+class ResumeState(TypedDict):
+    resume_text: str
+    skills_section: str
+    experience_section: str
+    projects_section: str
+    job_description: str
+    skills_score: float
+    experience_score: float
+    project_score: float
+    education_score: float
+    final_score: float
+    decision: str
+    analysis_summary: str

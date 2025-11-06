96abb1dfbd0ce0ea1b64dc1fab9aaa261c5385e4
Hardik-G14 <hardikgathibandhe14@gmail.com>
2025-11-06 23:26:30 +0530

    Added personal evaluator and parallel workflow

--- src/graph/state/graph_state.py
+++ src/graph/state/graph_state.py
@@ -1,20 +1,24 @@
 from typing import TypedDict
 from typing import TypedDict, Optional, List
 
-class ResumeState(TypedDict):
+class ResumeState(TypedDict, total = False):
     pdf_path: str
     resume_text: str
+
+    ## Sections
     skills_section: str
     experience_section: str
     projects_section: str
-    job_description: str
-    skills_score: float
-    experience_score: float
-    project_score: float
-    education_score: float
-    final_score: float
-    decision: str
-    analysis_summary: str
+    education_section: str
+    personal_section: str
+
+    ## Personal
+    name: str
+    email: str
+    phone_number: str
+    languages: Optional[List[str]]
+
+    ## Skills Evaluator
     skills_score: int
     matched_skills : Optional[List[str]]
     missing_skills : Optional[List[str]]
--- src/main.py
+++ src/main.py
@@ -1,34 +1,21 @@
 from src.graph.stategraph import create_graph
 
-# pdf_path: str
-#     resume_text: str
-#     skills_section: str
-#     experience_section: str
-#     projects_section: str
-#     job_description: str
-#     skills_score: float
-#     experience_score: float
-#     project_score: float
-#     education_score: float
-#     final_score: float
-#     decision: str
-#     analysis_summary: str
-#     skills_score: int
-#     matched_skills : Optional[List[str]]
-#     missing_skills : Optional[List[str]]
-#     role_inferred: str
-
-
 graph = create_graph()
 
 result = graph.invoke({
-    "pdf_path": "your own absolute path to pdf"
+    "pdf_path": "/Users/hardik/PycharmProjects/ResumeEvaluator/src/data/HardikResumeUpdated.pdf"
 })
 
 
-print(result["resume_text"])
+# print(result["resume_text"])
 print(result["skills_score"])
 print(result["role_inferred"])
 print(result["matched_skills"])
 print(result["missing_skills"])
+
+print("-----------")
+print(result["name"])
+print(result["email"])
+print(result["phone_number"])
+print(result["languages"])
 
